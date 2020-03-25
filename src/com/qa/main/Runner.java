33cf072da61bd86c5636b10fedaf57ef809e2aed
Nick Johnson <nickrstewarttds@gmail.com>
2020-03-25 17:28:15 +0000

    init commit

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,21 @@
+package com.qa.main;
+
+public class Runner {
+	public static void main(String[] args) {
+		int age = null;
+		System.out.println(age);
+
+		Integer cash = null;
+		System.out.println(cash);
+
+		String characters = "Hello";
+		System.out.println(characters.length());
+		System.out.println(characters.charAt(0));
+
+		double wallet = null;
+		System.out.println(wallet);
+
+		Double wallet2 = null;
+		System.out.println(wallet2);
+	}
+}
